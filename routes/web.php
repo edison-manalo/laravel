8cd07aadfa1d89ff92a48bfd71f2c17613a68915
Edison Manalo <manausedison@gmail.com>
2026-01-02 21:05:08 +0800

    Route resource

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
+/*
 Route::controller(JobController::class)->group(function () {
     Route::get('/jobs', 'index');
     Route::get('/jobs/create', 'create');
@@ -14,5 +15,15 @@ Route::controller(JobController::class)->group(function () {
     Route::patch('/jobs/{job}', 'update');
     Route::delete('/jobs/{job}', 'destroy');
 });
+*/
+
+/*
+ * Often you don't need to generate or register all seven resourceful routes,
+ * so you can pass an array as third item
+ * 
+ * ['only' => ['index', 'show', 'create', 'store']]
+ * ['except' => ['edit']]
+*/
+Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
